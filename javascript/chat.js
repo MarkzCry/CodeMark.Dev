e851077d66a32f11de5d5998c80ba96ef28975a0
Markel <83198538+MarkzCry@users.noreply.github.com>
2023-11-24 01:01:20 -0600

    Update chat.js

--- javascript/chat.js
+++ javascript/chat.js
@@ -1,33 +1,76 @@
+// Prompt for user's name when the page loads
+const username = prompt('Please enter your name:');
+// You might want to add validation to ensure a valid username is entered
+
+// Function to display messages in the chat UI with the associated username
 // Establish a Socket.io connection
 const socket = io("https://chatserver.codemarkapp.repl.co/");
-
-// Function to display messages in the chat UI
-function displayMessage(message) {
+function displayMessage(username, message) {
   const messageElement = document.createElement('div');
-  messageElement.textContent = message;
+  messageElement.innerHTML = `<strong>${username}:</strong> ${message}`;
   document.getElementById('chat-messages').appendChild(messageElement);
 }
+
+// Function to display typing indicator
+function displayTyping(username) {
+  const typingElement = document.createElement('div');
+  typingElement.innerHTML = `<em>${username} is typing...</em>`;
+  typingElement.setAttribute('data-typing', username);
+  typingElement.id = `typing-${username}`; // Unique ID for typing indicator
+  document.getElementById('chat-messages').appendChild(typingElement);
+}
+
+let typingTimer;
+const TYPING_DELAY = 1000; // Adjust the delay time as needed
+
+document.getElementById('message-input').addEventListener('input', () => {
+  clearTimeout(typingTimer);
+
+  if (!typingTimer) {
+    socket.emit('typing', username);
+  }
+
+  typingTimer = setTimeout(() => {
+    socket.emit('stopTyping', username);
+    typingTimer = null; // Reset typingTimer after emitting stopTyping event
+  }, TYPING_DELAY);
+});
+
+socket.on('userTyping', (user) => {
+  if (!document.getElementById(`typing-${user}`)) {
+    displayTyping(user);
+  }
+});
+
+socket.on('userStoppedTyping', (user) => {
+  const typingElement = document.getElementById(`typing-${user}`);
+  if (typingElement) {
+    typingElement.remove();
+  }
+});
+
+
 
 // Listen for initial messages from the server when the client connects
 socket.on('initialMessages', (messages) => {
   messages.forEach((messageObj) => {
-    displayMessage(messageObj.text);
+    displayMessage(messageObj.username, messageObj.text);
   });
 });
 
 // Event listener for sending messages via Fetch API
 document.getElementById('send-btn').addEventListener('click', () => {
   const message = document.getElementById('message-input').value.trim();
-  if (message !== '') {
+
+  if (message !== '' && username) {
     fetch('https://chatserver.codemarkapp.repl.co/sendMessage', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ message }),
+      body: JSON.stringify({ message, username }),
     })
       .then(response => {
-        // Handle the response from the server (if needed)
         if (response.ok) {
           console.log('Message sent successfully');
         } else {
@@ -37,11 +80,10 @@ document.getElementById('send-btn').addEventListener('click', () => {
       .catch(error => {
         console.error('Error:', error);
       });
-    document.getElementById('message-input').value = ''; // Clear input field after sending message
+    document.getElementById('message-input').value = '';
   }
 });
 
-// Listen for new messages broadcasted from the server using Socket.io
 socket.on('newMessage', (data) => {
-  displayMessage(data.message);
+  displayMessage(data.username, data.message);
 });
