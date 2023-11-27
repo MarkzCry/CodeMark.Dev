2adfeaaea81cb5f23f341f92928628b3e4cfec73
Markel <83198538+MarkzCry@users.noreply.github.com>
2023-11-27 13:05:49 -0600

    Update chat.js

--- javascript/chat.js
+++ javascript/chat.js
@@ -1,8 +1,8 @@
 // Prompt for user's name when the page loads
 let username = prompt('Please enter your name:');
 
-if(username == null) {
-  username = "User"
+if (username == null) {
+  username = "User";
 }
 // You might want to add validation to ensure a valid username is entered
 
@@ -10,9 +10,19 @@ if(username == null) {
 // Establish a Socket.io connection
 const socket = io("https://chatserver.codemarkapp.repl.co/");
 function displayMessage(username, message) {
+  const chatMessages = document.getElementById('chat-messages');
+  const isScrolledToBottom = chatMessages.scrollHeight - chatMessages.clientHeight <= chatMessages.scrollTop + 1;
+
   const messageElement = document.createElement('div');
+  messageElement.setAttribute('id', 'all-messages')
   messageElement.innerHTML = `<strong>${username}:</strong> ${message}`;
-  document.getElementById('chat-messages').appendChild(messageElement);
+  chatMessages.appendChild(messageElement);
+
+  if (isScrolledToBottom) {
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+  } else {
+    messageElement.scrollIntoView({ behavior: 'smooth', block: 'end' });
+  }
 }
 
 // Function to display typing indicator
@@ -53,8 +63,6 @@ socket.on('userStoppedTyping', (user) => {
   }
 });
 
-
-
 // Listen for initial messages from the server when the client connects
 socket.on('initialMessages', (messages) => {
   messages.forEach((messageObj) => {
@@ -62,7 +70,7 @@ socket.on('initialMessages', (messages) => {
   });
 });
 
-// Event listener for sending messages via Fetch API
+// Event listener for sending messages via Fetch API (on Send button click)
 document.getElementById('send-btn').addEventListener('click', () => {
   const message = document.getElementById('message-input').value.trim();
 
@@ -84,7 +92,40 @@ document.getElementById('send-btn').addEventListener('click', () => {
       .catch(error => {
         console.error('Error:', error);
       });
+
     document.getElementById('message-input').value = '';
+  }
+});
+
+// Event listener for sending messages via Fetch API (on Enter key press)
+document.getElementById('message-input').addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault(); // Prevent default behavior (like adding a new line)
+    
+    const message = document.getElementById('message-input').value.trim();
+
+    if (message !== '' && username) {
+      // Send the message here (similar to the code in your existing 'send-btn' click event)
+      fetch('https://chatserver.codemarkapp.repl.co/sendMessage', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ message, username }),
+      })
+      .then(response => {
+        if (response.ok) {
+          console.log('Message sent successfully');
+        } else {
+          console.error('Failed to send message');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
+      document.getElementById('message-input').value = ''; // Clear the input field after sending
+    }
   }
 });
 
