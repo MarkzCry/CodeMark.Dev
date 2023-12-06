2d781bc45c6d5b3fdefbb6bc89d55432aad27dc6
Markel <83198538+MarkzCry@users.noreply.github.com>
2023-12-06 08:38:34 -0600

    Update and rename about.js to projects.js

---
+++ javascript/projects.js
@@ -0,0 +1,76 @@
+// projects.js
+
+const projects = [
+    {
+        name: "Shouts",
+        description: "Shouts/Posts for the CodeMark website.",
+        link: "../tools/shouts",
+        category: "Tools",
+        icon: "./images/shouts.png"
+    },
+    {
+        name: "Post",
+        description: "Post your own shouts if you have an admin password.",
+        link: "../tools/post",
+        category: "Tools",
+        icon: "./images/postIcon.png"
+    },
+    {
+        name: "Gimkit Spawner",
+        description: "Spawns gimkit bots at the click of a button.",
+        link: "../hacks/gimkit-spawner",
+        category: "Hacks",
+        icon: "./images/gimkitIcon.png"
+    },
+    {
+        name: "Gimkit Hack",
+        description: "Instructions on how to hack gimkit in chrome.",
+        link: "./gimkit-hack",
+        category: "Hacks",
+        icon: "./images/gimkitIcon.png"
+    },
+    {
+        name: "Blooket Hack",
+        description: "Instructions on how to hack blooket within the dev console.",
+        link: "./blooket-hack",
+        category: "Hacks",
+        icon: "./images/blooketIcon.png"
+    },
+    {
+        name: "Type Test",
+        description: "Test your typing speed and accuracy.",
+        link: "../typetest",
+        category: "Games",
+        icon: "./images/typingIcon.png"
+    },
+    {
+        name: "Snake",
+        description: "A fun snake game.",
+        link: "../games/snake",
+        category: "Games",
+        icon: "./images/snakeIcon.png"
+    },
+    {
+        name: "AI GPT-3.5",
+        description: "Talk to AI",
+        link: "../ai",
+        category: "Tools",
+        icon: "./images/aiIcon.png"
+    },
+    {
+        name: "Youtube Videos",
+        description: "Displays the last 3 latest videos.",
+        link: "../youtube",
+        category: "Other Pages",
+        icon: "./images/youtube.png"
+    },
+    {
+        name: "Chat",
+        description: "Chat with random people, with a random username.",
+        link: "../chat",
+        category: "Tools",
+        icon: "./images/chatIcon.png"
+    },
+];
+
+window.projectsData = projects;
