11f254528cb0663b6872cd04486852c67712c776
Markel <83198538+MarkzCry@users.noreply.github.com>
2024-04-09 19:36:19 -0500

    Update config.js

--- javascript/config.js
+++ javascript/config.js
@@ -1,4 +1,4 @@
-const serverEndpoint = "https://77e34fbb-ff4d-417b-afb8-b0e3359aa07a-00-3l9m8oqoi94ju.janeway.replit.dev/";
+const serverEndpoint = "https://server.codemark.app/";
 
 window.config = {
   serverEndpoint: serverEndpoint
